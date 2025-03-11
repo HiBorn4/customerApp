4a919dc7e816312cecb6f3131282a74436661af5
aryanNF <aryan@nativefarmers.co.in>
2025-03-11 20:46:39 +0530

    integrated firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,12 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+plugins {
+
+  // Add the dependency for the Google services Gradle plugin
+  id("com.google.gms.google-services") version "4.4.2" apply false
 
 }
 
